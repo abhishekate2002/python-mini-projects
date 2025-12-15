a70b343040d0d31e55c39f35563ecc78bede4c87
Abhishek Kate <65706743+abhishekate2002@users.noreply.github.com>
2025-12-15 17:49:45 +0530

    Add list of trivia questions and answers
    Added a list of trivia questions with answers related to heights, weights, speeds, and other interesting facts.

---
+++ higher_lower/questions.py
@@ -0,0 +1,110 @@
+list_of_questions =[
+{
+"question": "Height of the Burj Khalifa in meters",
+"answer": 828
+},
+{
+"question": "Height of Merdeka 118 in meters",
+"answer": 679
+},
+{
+"question": "Height of Shanghai Tower in meters",
+"answer": 632
+},
+{
+"question": "Height of Makkah Royal Clock Tower in meters",
+"answer": 601
+},
+{
+"question": "Height of Ping An Finance Center in meters",
+"answer": 599
+},
+{
+"question": "Maximum weight of a blue whale in tonnes",
+"answer": 190
+},
+{
+"question": "Maximum weight of an African bush elephant in tonnes",
+"answer": 10
+},
+{
+"question": "Maximum weight of a hippopotamus in tonnes",
+"answer": 4
+},
+{
+"question": "Top speed of a cheetah in km/h",
+"answer": 120
+},
+{
+"question": "Top speed of a pronghorn antelope in km/h",
+"answer": 98
+},
+{
+"question": "Top speed of a lion in km/h",
+"answer": 80
+},
+{
+"question": "Height of Mount Everest in meters",
+"answer": 8848
+},
+{
+"question": "Height of the Eiffel Tower in meters",
+"answer": 324
+},
+{
+"question": "Length of the Great Wall of China in km (total)",
+"answer": 21196
+},
+{
+"question": "Number of bones in an adult human body",
+"answer": 206
+},
+{
+"question": "Height difference: Eiffel Tower taller than Washington Monument in feet",
+"answer": 300
+},
+{
+"question": "Tallest unicycle ever mastered in feet",
+"answer": 46
+},
+{
+"question": "Number of M&Ms in a one-pound bag",
+"answer": 425
+},
+{
+"question": "Lifespan of a queen ant in years",
+"answer": 7
+},
+{
+"question": "Annual migration distance of a box turtle in miles",
+"answer": 100
+},
+{
+"question": "Number of vertebrae in giraffe neck",
+"answer": 7
+},
+{
+"question": "Average cruising altitude of commercial airplane in feet",
+"answer": 36000
+},
+{
+"question": "Length of the Amazon River in km",
+"answer": 6992
+},
+{
+"question": "Height of Statue of Liberty in meters",
+"answer": 93
+},
+{
+"question": "Weight of the largest recorded giant squid in kg",
+"answer": 275
+},
+{
+"question": "Number of keys on a standard piano",
+"answer": 88
+},
+{
+"question": "Length of the Blue Whale in meters (average max)",
+"answer": 30
+}
+]
